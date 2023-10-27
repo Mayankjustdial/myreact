1bae40210e462e3504f4185a550f49224d294624
Mayank <mayankghz225@gmail.com>
2023-10-27 14:39:34 +0530

    Embedding expression in JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <h1>Welcome to react</h1>
+      <p>This is how we create react jsx element</p>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/component/Message.js
@@ -0,0 +1,11 @@
+const Message = () => {
+  const name = "John";
+  const age = 30;
+  return (
+    <h1>
+      Good Morning {name}, your age is {30}
+    </h1>
+  );
+};
+
+export default Message;
